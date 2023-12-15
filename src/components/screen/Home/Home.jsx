0c2a0f1a4ae8d3ce27153d9f8465ad60d5c088db
redmi4ek <radmirsolo@gmail.com>
2023-12-15 10:16:25 +0600

    dada

--- src/components/screen/Home/Home.jsx
+++ src/components/screen/Home/Home.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./item/TodoItem";
-
-const todos =[
+import CreateTodoField from "./create-todo-field/CreateTodoField";
+const data =[
 {
     id: 'wewew',
     title: 'asadad',
@@ -20,12 +20,32 @@ const todos =[
 ]
 
 const Home = () => {
+    const [todos, setTodos] = useState(data) // наши состояния нельзя мутировать  
+
+    const changeTodo = _id => {
+        const copy = [...todos]
+        const current = copy.find(t => t.id === _id) // туду по id которые мы дали 
+        current.isCompleted = !current.isCompleted  // грубо переворачивает из тру в фолс
+        setTodos(copy) // меняем значение  
+    }
+    const deleteTodo = _id => setTodos(prevTodos => prevTodos.filter(t => t.id !== _id));
+ 
+    
+
+
+    
+
   return (
     <div className=' text-white w-4/5 mx-auto'>
       <h1 className='text-2xl font-bold text-center mb-4'>Todo</h1>  
       {todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} 
+          todo={todo} 
+          changeTodo={changeTodo}
+          deleteTodo={deleteTodo}
+        />
     ))}
+      <CreateTodoField setTodos={setTodos} />
     </div>
   )
 }
--- src/components/screen/Home/item/TodoItem.jsx
+++ src/components/screen/Home/item/TodoItem.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Check from './Check'
+import { BsTrash } from 'react-icons/bs'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo , changeTodo, deleteTodo }) => {
   return (
-    <button className=' flex items-center mb-4 rounded-xl bg-gray-800 p-5 
-    w-full'>
-        <Check/>
-        {todo.title}</button>
+    <div 
+      className=' flex items-center justify-between mb-4 
+      rounded-2xl bg-gray-800 p-5 w-full'>
+        <button className=' flex items-center ' 
+          onClick={() => changeTodo(todo.id)}>       
+        <Check isCompleted={todo.isCompleted}/>
+        <span className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.title}</span>
+      </button>
+      <button onClick={() => deleteTodo(todo.id)}>
+        <BsTrash size={22} className=' text-gray-900  
+                                hover:text-red-700'/>
+      </button>
+    </div>
   )
 }
 
